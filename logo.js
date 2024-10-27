400c858be12a6053139ba2bbf2b8cfe087cd2ac2
Ridam Veer Singh Gulati <officialrvsg@gmail.com>
2024-10-27 19:56:27 +0530

    Add files via upload

---
+++ logo.js
@@ -0,0 +1,7 @@
+
+  document.addEventListener("DOMContentLoaded", function() {
+    let link = document.createElement("link");
+    link.rel = "icon";
+    link.href = "img/logo.png";
+    document.head.appendChild(link);
+  });
